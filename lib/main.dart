cd7d0f80852c5986381173f4ae6a33538514e3a9
fabiohc <fabiohcampos09@gmail.com>
2019-05-11 13:39:19 -0300

    Istilização layout

--- lib/main.dart
+++ lib/main.dart
@@ -25,12 +25,32 @@ class _HomeState extends State<Home> {
       ],
     );
 
+    Icon icon = Icon(Icons.wb_sunny, size: 120, color: Colors.yellow,);
+
+    TextStyle styleDecoration = TextStyle(color: Colors.blueAccent,fontSize: 20);
+    TextStyle styleField = TextStyle(color: Colors.blueAccent);
+
+    RaisedButton raisedButton = RaisedButton(
+        onPressed: (){},
+      child: Text("Cacular"),
+      color: Colors.blueAccent,
+    );
+
+    TextField tempCelsius = TextField(keyboardType: TextInputType.number,
+    decoration: InputDecoration(labelText: "Temperatura em graus Celsius"),
+      textAlign: TextAlign.center,
+    );
+    TextField tempFahreheit = TextField(keyboardType: TextInputType.number,
+      decoration: InputDecoration(labelText: "Temperatura em graus Fahreheit"),
+      textAlign: TextAlign.center,
+    );
+
     Column column = Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-
+        icon, tempCelsius, tempFahreheit,raisedButton
       ],
-    )
+    );
     
     Scaffold scaffold = Scaffold(
     appBar: appBar,
