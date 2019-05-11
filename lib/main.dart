8234049ccfe09350894129283d2830d4f7000854
fabiohc <fabiohcampos09@gmail.com>
2019-05-11 14:55:04 -0300

    adicionando controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main () => runApp(MaterialApp(
-    home: Home()
-));
+void main() => runApp(MaterialApp(home: Home()));
 
 class Home extends StatefulWidget {
   @override
@@ -16,38 +14,45 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-
-    void _reserFields(){
+    void _reserFields() {
       celsiusController.text = "";
       fahrenheitController.text = "";
     }
 
-    void _converter(){
+    void _converter() {
       double celsius = double.parse(celsiusController.text);
       double fahrenheit = celsius * 1.8 + 32.0;
       fahrenheitController.text = fahrenheit.toStringAsFixed(4);
     }
 
     AppBar appBar = AppBar(
-      
       title: Text("Conversor de Temperatura"),
       centerTitle: true,
       backgroundColor: Colors.blue,
       actions: <Widget>[
         IconButton(
           icon: Icon(Icons.refresh),
-          onPressed: _reserFields,),
+          onPressed: _reserFields,
+        ),
       ],
     );
 
-    Icon icon = Icon(Icons.wb_sunny, size: 120, color: Colors.yellow,);
+    Icon icon = Icon(
+      Icons.wb_sunny,
+      size: 120,
+      color: Colors.yellow,
+    );
 
-    TextStyle styleDecoration = TextStyle(color: Colors.blueAccent,fontSize: 20);
+    TextStyle styleDecoration =
+        TextStyle(color: Colors.blueAccent, fontSize: 20);
     TextStyle styleField = TextStyle(color: Colors.blueAccent);
 
-
     RaisedButton raisedButton = RaisedButton(
-        onPressed: _converter,
+      onPressed: () {
+        if (_formKey.currentState.validate()) {
+          _converter();
+        }
+      },
       child: Text("Cacular"),
       color: Colors.blueAccent,
     );
@@ -57,53 +62,60 @@ class _HomeState extends State<Home> {
       child: raisedButton,
     );
 
-    Padding padding = Padding(padding: EdgeInsets.only(top: 20.0),
-    child: containerBtn,
+    Padding padding = Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: containerBtn,
     );
 
-    TextField tempCelsius = TextField(keyboardType: TextInputType.number,
-    decoration: InputDecoration(
-      labelText: "Temperatura em graus Celsius",
-      labelStyle : styleDecoration,
-    ),
-    textAlign: TextAlign.center,
-    style: styleField,
-    controller: celsiusController,
-    );
-
-    TextField tempFahreheit = TextField(keyboardType: TextInputType.number,
+    TextField tempCelsius = TextField(
+      keyboardType: TextInputType.number,
       decoration: InputDecoration(
-          labelText: "Temperatura em graus Fahreheit",
-      labelStyle: styleDecoration,
+        labelText: "Temperatura em graus Celsius",
+        labelStyle: styleDecoration,
+      ),
+      textAlign: TextAlign.center,
+      style: styleField,
+      controller: celsiusController,
+      /* validador: (value){
+        if (value.isEmpty){
+          return "Informe um valor";
+        }
+      },*/
+    );
+
+    TextField tempFahreheit = TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: "Temperatura em graus Fahreheit",
+        labelStyle: styleDecoration,
       ),
       textAlign: TextAlign.center,
       style: styleField,
       controller: fahrenheitController,
+      /*validador: (value){
+          if (value.isEmpty){
+            return "Informe um valor";
+          }
+        },*/
     );
 
     Column column = Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        icon, tempCelsius, tempFahreheit,padding
-      ],
+      children: <Widget>[icon, tempCelsius, tempFahreheit, padding],
     );
 
-    Form form = Form(
-        child: column,
-        key: _formKey
-    );
+    Form form = Form(child: column, key: _formKey);
 
     SingleChildScrollView singleChildScrollView = SingleChildScrollView(
-      child: column,
+      child: form,
       padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
     );
-    
+
     Scaffold scaffold = Scaffold(
-    appBar: appBar,
-      backgroundColor: Colors.white,
-      body: singleChildScrollView
-    );
-    
+        appBar: appBar,
+        backgroundColor: Colors.white,
+        body: singleChildScrollView);
+
     return scaffold;
   }
 }
